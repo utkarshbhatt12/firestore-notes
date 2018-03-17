2527bbf2b383e9b534e0edd271382c2b4d58c4b0
Utkarsh Bhatt <utkarsh.bhatt12@gmail.com>
2018-03-17 17:32:28 +0530

    added some comments to the code

--- js/firebase/__init.js
+++ js/firebase/__init.js
@@ -1,6 +1,6 @@
 'use strict';
-// Firebase stuff. You can find this on
-// console.firebase.com
+
+// firebase stuff. You can find this in the https://console.firebase.google.com page
 firebase.initializeApp({
     apiKey: "AIzaSyByLHLc7CLtlsimXHLNWGsbV4eiGuZIE4A",
     authDomain: "contactform-1b262.firebaseapp.com",
@@ -10,3 +10,35 @@ firebase.initializeApp({
 
 // reference to an instance of Firebase Firestore
 const db = firebase.firestore();
+
+
+/**
+ * Writes the note to the firestore database creating a document inside the 'notes' collection
+ * @param {object} note object containing the note title, body and the event timestamp
+ */
+const sendNote = (note) => {
+
+    // a reference to the notes collection inside the default firestore database
+    const notesRef = db.collection('notes');
+
+
+    notesRef.doc().set(note)
+        .then(() => {
+            // updates the status after the note is created in the firestore
+            statusSpan.innerText = 'note created';
+            sessionStorage.clear();
+        }).catch(error => {
+
+            // something went wrong. Make sure that you have set up the permissions 
+            // to allow anyone to write to the Firestore
+            // READ MORE: https://firebase.google.com/docs/firestore/security/get-started
+            console.log(error);
+            statusSpan.innerText = 'oops... something went wrong!';
+        });
+
+    // set the status to idle after the note is published since 
+    // the app is doing nothing after that
+    setTimeout(() => {
+        statusSpan.innerText = 'idle...';
+    }, 2000);
+};
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Firestore Notes App
+
+This is a very simple notes app which uses the Firestore database to store notes.
